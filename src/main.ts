045e508e3aed9186db8dc68708ef7ea08254c7fb
Francisco Rosemberg <frvgomes@gmail.com>
2023-08-31 07:49:39 -0300

    Parametrização Inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    //No exemplo da aula o cara usou module: AppModule e httpAdapter:new FastifyAdapter()
+    // mesmo que não documentação esteja igual o exemplo que fiz.
+  );
+  await app.listen(3000, '0.0.0.0');
+  // Aqui também o cara usou address:'0.0.0.0'
 }
 bootstrap();
