180b3e31008cfcae8ed1a0ef1cce25c8ecd58f09
Francisco Rosemberg <frvgomes@gmail.com>
2023-09-01 08:22:55 -0300

    Testes unitários - users getters

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -20,4 +20,27 @@ describe('UserEntity unit tests', () => {
     expect(userEntity.props.password).toEqual(props.password);
     expect(userEntity.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('Getter of name field', () => {
+    expect(userEntity.props.name).toBeDefined();
+    expect(userEntity.props.name).toEqual(props.name);
+    expect(typeof userEntity.props.name).toEqual('string');
+  });
+
+  it('Getter of email field', () => {
+    expect(userEntity.props.email).toBeDefined();
+    expect(userEntity.props.email).toEqual(props.email);
+    expect(typeof userEntity.props.email).toEqual('string');
+  });
+
+  it('Getter of password field', () => {
+    expect(userEntity.props.password).toBeDefined();
+    expect(userEntity.props.password).toEqual(props.password);
+    expect(typeof userEntity.props.password).toEqual('string');
+  });
+
+  it('Getter of createdAt field', () => {
+    expect(userEntity.props.createdAt).toBeDefined();
+    expect(userEntity.props.createdAt).toBeInstanceOf(Date);
+  });
 });
