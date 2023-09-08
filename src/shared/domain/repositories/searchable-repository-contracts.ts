47474480de6a0c6b7f1eda0d8bf47a0ba92b17e3
Francisco Rosemberg <frvgomes@gmail.com>
2023-09-08 08:09:40 -0300

    Adicionando SearchableRepositoryInterface - parte 2

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -1,10 +1,35 @@
 import { Entity } from '../entities/entity';
 import { RepositoryInterface } from './repository-contracts';
 
+export type SortDirection = 'asc' | 'desc';
+
+export type SearchProps<Filter = string> = {
+  page?: number;
+  perPage?: number;
+  sort?: string | null;
+  sortDir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
+export class SearchParams {
+  protected _page: number;
+  protected perPage = 15;
+  protected sort: string | null;
+  protected sortDir: SortDirection | null;
+  protected filter: string | null;
+  constructor(props: SearchProps) {
+    this._page = props.page;
+    this.perPage = props.perPage;
+    this.sort = props.sort;
+    this.sortDir = props.sortDir;
+    this.filter = props.filter;
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
   SearchInput,
   SearchOutput,
 > extends RepositoryInterface<E> {
-  search(props: SearchInput): Promise<SearchOutput>;
+  search(props: SearchParams): Promise<SearchOutput>;
 }
