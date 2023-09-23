756638726bf67fff46f85c58a7e5150031eeec94
Francisco Rosemberg <frvgomes@gmail.com>
2023-09-23 16:00:41 -0300

    Adding ListUserUserCase - par 1

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,5 +1,5 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
-import { BadRequetError } from '../errors/bad-request-error';
+import { BadRequetError } from '../../../shared/application/errors/bad-request-error';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { HashProvider } from '@/shared/application/providers/hash-provider';
 import { UserOutput } from '../dtos/user-output';
