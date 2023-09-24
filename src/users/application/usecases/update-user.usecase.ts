14d8fda7c6fa6a9ccc322ba90eb847599bb43bbf
Francisco Rosemberg <frvgomes@gmail.com>
2023-09-24 08:25:45 -0300

    Adding UpdateUserUsecase

--- src/users/application/usecases/update-user.usecase.ts
+++ src/users/application/usecases/update-user.usecase.ts
@@ -1,10 +1,12 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
+import { BadRequetError } from '@/shared/application/errors/bad-request-error';
 
-export namespace GetUserUseCase {
+export namespace UpdateUserUseCase {
   export type Input = {
     id: string;
+    name: string;
   };
 
   export type Output = UserOutput;
@@ -13,8 +15,15 @@ export namespace GetUserUseCase {
     constructor(private userRepository: UserRepository.Repository) {}
 
     async execute(input: Input): Promise<Output> {
+      if (!input.name) {
+        throw new BadRequetError('Name not provided');
+      }
+
       const entity = await this.userRepository.findById(input.id);
 
+      entity.update(input.name);
+
+      await this.userRepository.update(entity);
       return UserOutputMapper.toOutput(entity);
     }
   }
